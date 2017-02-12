e9972e95c3cd25cf2832990624829df304aad3bc
David Brockman Smoliansky <david@springworks.se>
2017-02-12 23:33:49 +0100

    remove comment

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 'use strict';
 
-/**
- * Estimate the Earth radius at given latitude.
- *
- * This function is based on the equation discussed on this GIS Stack Exchange thread:
- * http://gis.stackexchange.com/questions/20200
- * And this wikipedia entry on Geocentric Earth radius:
- * http://en.wikipedia.org/wiki/Earth_radius#Geocentric_radius
- *
- * The equation:
- * R = ( (a^2 cos(f))^2 + (b^2 sin(f))^2 ) / ( (a cos(f))^2 + (b sin(f))^2 )
- * where `f` is the latitude and `a` and `b` are the
- * equatorial radius and the polar radius, respectively.
- *
- * @param  {Number}  latitude  The latitude.
- * @return  {Number}  Radius in meters.
- */
 function earthRadiusAtGeodeticLatitude(latitude) {
   const r_major = 6378137;
 
@@ -37,6 +21,5 @@ function earthRadiusAtGeodeticLatitude(latitude) {
     )
   );
 }
-
 
 module.exports = earthRadiusAtGeodeticLatitude;
